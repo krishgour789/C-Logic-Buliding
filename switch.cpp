0e4326c4b4424766009e28af1ee0f80f58f2b33c
krishgour789 <krishgour789@gmail.com>
2025-06-11 13:34:39 -0800

    switch

---
+++ switch.cpp
@@ -0,0 +1,54 @@
+
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int menu;
+//     cout<<"Give orders 1.paneer 2.chai 3. coffe";
+//     cin>>menu;
+//     switch(menu)
+//     {
+//     case 1:cout<<"paneer";
+//     break;
+//     case 2:cout<<"chai";
+//     break;
+//     case 3: cout<<"coffess";
+//     break;
+//     default:cout<<"ip;";
+//     }
+// }
+
+//go to is use to skip the program and also kknow as jumping
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"krish";
+//     cout<<"rishabh";
+//     goto sp;
+//     cout<<"yash";
+//     sp:
+   
+//     cout<<"jai";
+// }   
+
+
+// reverse jumping.....
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"krish";
+//     cout<<"rishabh";
+//     int i=1;
+//     sp:
+//     if(i<6)
+//     {
+//     cout<<"yash";
+//     ++i;
+//     goto sp;
+//     }
+//     cout<<"jai";
+    
+// }
