e4d7e09fb55b86cf3e1faf112de20887e956bb98
krishgour789 <krishgour789@gmail.com>
2025-06-10 21:30:23 -0800

    logics questions

---
+++ day_1logics.cpp
@@ -0,0 +1,128 @@
+// -----------------5 Logic building Question(C++)----------------
+// Q1 To check which number is Greater?
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"Enter the First Number ;";
+//     cin>>a;
+//     cout<<"Enter the second number ;";
+//     cin>>b;
+//     if(a>=b)
+//     {
+//         cout<<"the largest number is "<<a<<endl;
+        
+//     }
+//     else{
+//         cout<<"the laregst number is"<<b<<endl;
+//     }
+// }
+
+// Q2. To chcek the Grade Marks
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int marks;
+//     cout<<"Enter the Marks ;";
+//     cin>>marks;
+    
+//     if(marks<0 || marks>100)
+//     {
+//         cout<<"Inavlid Marks , write Between 0 to 100 "<<endl;
+        
+//     }
+//     else if(marks>=80)
+//     {
+//         cout<<"A+ Grade";
+//     }
+//     else if(marks>=70)
+//     {
+//         cout<<"B+ Grade";
+//     }
+//     else if(marks>=60)
+//     {
+//         cout<<"C+ Grade";
+//     }
+//     else if(marks>=40)
+//     {
+//         cout<<"E+ Grade";
+//     }
+        
+    
+// }
+
+// 3. To use the operators(+,-,*,/)
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     char ch;
+//     cout<<"Enter the Number";
+//     cin>>a;
+//     cout<<"enter the number;";
+//     cin>>b;
+//     cout<<"enter the operator:";
+//     cin>>ch;
+    
+//     if(ch=='+')
+//     {
+//         cout<<a+b;
+//     }
+//     else if(ch=='-')
+//     {
+//         cout<<a-b;
+//     }
+//     else if(ch=='*')
+//     {
+//         cout<<a*b;
+//     }
+//     else if(ch=='/')
+//     {
+//         cout<<a/b;
+//     }
+//     else{
+//         cout<<"Please Selecet the Right Operato(+,-,*,/";
+//     }
+// }
+// Q4 . This code is for to check the vowels and consonents
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char  ch;
+//     cout<<"Enter The Letters";
+//     cin>>ch;
+    
+//     if(ch=='a' || ch == 'e' || ch =='i' || ch == 'o' || ch == 'u')
+//     {
+//         cout<<"its vowel";
+        
+//     }
+//     else{
+//         cout<<"Consonent";
+//     }
+    
+// }
+    
+    
+// }
+// Qustion - Write a Program with the help of else if (vote)
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int age;
+//     cout<<"enter your age";
+//     cin>>age;
+    
+//     if(age>=18)
+//     {
+//         cout<<"You Can Vote Now";
+//     }
+//     else{
+//         cout<<"Grow Now --You are adult";
+//     }
+// }
